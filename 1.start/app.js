78f9f1755f2eedd5e7006d77b08d843f97573c53
Yura <ybeliaev@gmail.com>
2017-09-14 19:48:24 +0300

    start server

---
+++ 1.start/app.js
@@ -0,0 +1,11 @@
+﻿const http = require('http');
+const fs = require('fs');
+
+http.createServer(function (req, res) {
+    res.writeHead(200,{'Content-Type': 'text/html'});//'text/plain' не обрабатывает html разметку;
+    res.write('Hello World\n');
+    fs.readFile('index.html', function(err, content){
+    	res.write(decodeURIComponent(content));
+    	res.end();
+	})
+}).listen(3000,'127.0.0.1');
