c9f4bc5e361e1caedc7ef3811d8144bf7ef723dd
Yura <ybeliaev@gmail.com>
2017-09-14 19:48:52 +0300

    event

---
+++ 2.event/app.js
@@ -0,0 +1,12 @@
+﻿const http = require('http');
+const server = http.createServer().listen(3000, '127.0.0.1');
+
+server.on('request', function (req, res) {
+    res.writeHead(200);
+    res.write('Hello\n');
+    res.end('The end')
+})
+
+server.on('listening', function(){
+    console.log('Listen: 3000...')
+})
